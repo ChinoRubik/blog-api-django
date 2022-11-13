c11fc2838af85df450b9c9dd06c8b37a991937ad
Cristian Hernandez <chinorubikguitar@hotmail.com>
2022-11-12 19:28:24 -0600

    a

--- blog/settings.py
+++ blog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-n0n3j%a8n8te0em@@$n!ycc!+qypvx69+c9vijw4cdlfp58e7$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
